b4a9d020f74b758d0f86360d3d307a19b3fe26f4
Dinesh <dineshveguru123@gmail.com>
2022-10-01 15:52:04 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReactToPrint } from "react-to-print";
+import Resume from "./components/Resume";
+import React, { useRef } from "react";
 
 function App() {
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Resume innerRef={componentRef} />
+      <button onClick={handlePrint}>print me</button>
     </div>
   );
 }
---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import "../styles/resume.css";
+
+function Resume(props) {
+  return (
+    <div>
+      <div className="resume-container" ref={props.innerRef}>
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
